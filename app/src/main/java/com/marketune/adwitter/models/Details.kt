e1cd228f8dfe7b7e18a3e7fc3c6bd34f36449c44
Abdel-Rahman_El-Shikh <a.rahmanelshikh@gmail.com>
2019-11-06 17:11:19 +0200

    get user details from backend and bind it into the editText in bank info screen.

--- app/src/main/java/com/marketune/adwitter/models/Details.kt
+++ app/src/main/java/com/marketune/adwitter/models/Details.kt
@@ -3,12 +3,12 @@ package com.marketune.adwitter.models
 import com.google.gson.annotations.SerializedName
 
 data class Details(
-    @SerializedName("account_number") val account_number: String,
-    @SerializedName("bank_country") val bank_country: String,
-    @SerializedName("bank_name") val bank_name: String,
-    @SerializedName("beneficiary_name") val beneficiary_name: String,
-    @SerializedName("iban") val iban: String,
-    @SerializedName("id") val id: Int,
-    @SerializedName("phone") val phone: String,
-    @SerializedName("user_id") val user_id: Int
+    @SerializedName("account_number") var account_number: String,
+    @SerializedName("bank_country") var bank_country: String,
+    @SerializedName("bank_name") var bank_name: String,
+    @SerializedName("beneficiary_name") var beneficiary_name: String,
+    @SerializedName("iban") var iban: String,
+    @SerializedName("id") var id: Int,
+    @SerializedName("phone") var phone: String,
+    @SerializedName("user_id") var user_id: Int
 )
